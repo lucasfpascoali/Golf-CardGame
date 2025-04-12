7ab35a8a886aa8593ca4bf93f4dc6f2b9273f9b2
juliana <julianamirbosio@gmail.com>
2025-04-12 17:31:58 -0300

    login frame and game frame with side frame

--- src/player_interface.py
+++ src/player_interface.py
@@ -5,10 +5,10 @@ from PIL import Image, ImageTk
 class PlayerInterface:
     def __init__(self):
         # Colors
-        self._bg_color = "#0F3F27"
-        self._primary_color = "#5CA35F"
-        self._side_color = "#B7C3AD"
-        self._secondary_color = "#DBE0D2"
+        self._bg_color = "#0F3F27"          # verde-escuro
+        self._primary_color = "#5CA35F"     # verde-vivo
+        self._side_color = "#B7C3AD"        # verde-clarissimo-acizentado
+        self._secondary_color = "#DBE0D2"   # branco-gelo
 
         self.window = tk.Tk()
         self.window.title("Golf")
@@ -23,25 +23,20 @@ class PlayerInterface:
         self.window.mainloop()
 
     def _init_login_frame(self):
-        # Cria o frame de login ocupando a janela toda
         self.login_frame = tk.Frame(self.window, bg=self._primary_color)
         self.login_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
-
-        # Frame de layout horizontal (logo à esquerda, entrada à direita)
-        layout_frame = tk.Frame(self.login_frame, bg=self._primary_color)
-        layout_frame.place(relx=0.5, rely=0.5, anchor="center")
+        self.window.update()
 
         # Logo
         img = Image.open("assets/others/logo.png") 
-        img = img.resize((470, 503))
+        img = img.resize((500, 633))
         self.logo_img = ImageTk.PhotoImage(img)
-
-        logo_label = tk.Label(layout_frame, image=self.logo_img, bg=self._primary_color)
-        logo_label.grid(row=0, column=0, padx=50)
+        logo_label = tk.Label(self.login_frame, image=self.logo_img, bg=self._primary_color)
+        logo_label.place(x=220, y=195)
 
         # Campo de entrada e botão
-        right_frame = tk.Frame(layout_frame, bg=self._primary_color)
-        right_frame.grid(row=0, column=1, padx=20)
+        right_frame = tk.Frame(self.login_frame, bg=self._primary_color)
+        right_frame.place(x=868, y=412)
 
         self.nickname_entry = tk.Entry(
             right_frame,
@@ -49,8 +44,8 @@ class PlayerInterface:
             width=20,
             justify="center",
             bd=0,
-            bg="#E6E9DD",
-            fg="#A9A9A9"
+            bg=self._secondary_color,
+            fg=self._side_color
         )
         self.nickname_entry.insert(0, "Nickname")
         self.nickname_entry.pack(pady=10, ipady=10)
@@ -86,39 +81,32 @@ class PlayerInterface:
             self.login_frame.destroy()
 
         self.game_frame = tk.Frame(self.window, bg=self._bg_color)
-        self.game_frame.pack(fill="both", expand=True)
-        
-        # frame esquerdo
-        self._create_side_frame(nickname)
+        self.game_frame.place(x=0, y=0, width=1440, height=1024)
+
+        self._create_side_frame(self.game_frame)
 
         # Parte central (temporária)
-        content = tk.Frame(self.game_frame, bg=self._bg_color)
-        content.pack(side="right", fill="both", expand=True)
+        #content = tk.Frame(self.game_frame, bg=self._bg_color)
+        #content.pack(side="right", fill="both", expand=True)
 
-        label = tk.Label(
-            content,
-            text=f"Bem-vindo, {nickname}!",
-            fg="white",
-            bg=self._bg_color,
-            font=("Helvetica", 20)
-        )
-        label.pack(pady=40)
 
-    def _create_side_frame(self):
-        self._side_frame = tk.Frame(self.main_window, width=295, bg=self._side_color)
-        self._side_frame.pack(side="left", fill="y")
-        self._create_logo()
-        self._create_round_label(0)
-        self._create_players_label(3)
 
-    def _create_logo(self):
-        self._logo_img = tk.PhotoImage(file="src/assets/others/logo.png")
-        label_logo = tk.Label(self._side_frame, image=self._logo_img, bg=self._side_color)
-        label_logo.pack(padx=10, pady=40)
+    def _create_side_frame(self, game_frame):
+        self._side_frame = tk.Frame(game_frame, height=1024, width=295, bg=self._side_color)
+        self._side_frame.place(x=0, y=0)
+        self._create_logo(self._side_frame)
+        self._create_round_label(0, self._side_frame)
+        self._create_players_label(3, self._side_frame)
+        self._create_player_turn_label(1, self._side_frame)
 
-    def _create_round_label(self, round_number: int):
+    def _create_logo(self, _side_frame):
+        self._logo_img = tk.PhotoImage(file="assets/others/logo.png")
+        label_logo = tk.Label(_side_frame, image=self._logo_img, bg=self._side_color)
+        label_logo.place(x=26, y=54)
+
+    def _create_round_label(self, round_number: int, _side_frame):
         round_label = tk.Label(
-            self._side_frame,
+            _side_frame,
             text=f"Rodada {round_number}/9",
             bg=self._primary_color,
             fg="white",
@@ -127,37 +115,43 @@ class PlayerInterface:
             pady=10,
             width=20
         )
-        round_label.pack(pady=10)
+        round_label.place(x=0, y=439, width=295, height=33)
     
-    def _create_players_label(self, n_players: int):
-        players_label: list[tk.Label] = []
-        for player in range(n_players):
-            players_label.append(tk.Label(
-                self._side_frame,
-                text=f"Jogador {player}",
+    def _create_players_label(self, n_players: int, _side_frame):
+        for i in range(n_players):
+            player_label = tk.Label(
+                _side_frame,
+                text=f"Jogador {i + 1}: 0 pts",
                 bg=self._secondary_color,
-                fg="black",
-                font=("Inter", 14, "bold"),
+                fg=self._bg_color,
+                font=("Inter", 12, "bold"),
                 padx=20,
                 pady=5,
                 width=20
-            ))
-            players_label[player].pack(pady=5)
+            )
+            # Cada label 50px abaixo do anterior, começando em y=200 (exemplo)
+            player_label.place(x=0, y=486 + i * 40, width=295, height=33)
     
-
-    def _create_player_turn_label(self, player_number: int):
+    def _create_player_turn_label(self, player_number: int, _side_frame):
+        text = tk.Label(
+            _side_frame, 
+            text="É a vez de:", 
+            font=("Inter", 12), 
+            fg="white", 
+            bg=self._side_color
+        )
+        text.place(x=58, y=828)
         player_turn_label = tk.Label(
-            self._side_frame,
-            text=f"Vez do Jogador {player_number}",
+            _side_frame,
+            text=f"Jogador {player_number}",
             bg=self._primary_color,
-            fg="white",
+            fg=self._bg_color,
             font=("Inter", 14, "bold"),
             padx=20,
             pady=10,
             width=20
         )
-        player_turn_label.pack(pady=20)
-
+        player_turn_label.place(x=0, y=853, width=295, height=33)
 
     def _clear_placeholder(self, event):
         if self.nickname_entry.get() == "Nickname":
@@ -176,3 +170,6 @@ class PlayerInterface:
             return
         # Simula confirmação do servidor
         self._init_game_frame(nickname)
+
+if __name__ == "__main__":
+    PlayerInterface()
